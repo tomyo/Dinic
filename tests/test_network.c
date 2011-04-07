715b6acb51e54ca6ed1eef289991263e33237e9e
j0hn <j0hn.com.ar@gmail.com>
2011-04-07 20:09:33 -0300

    agregados tests de network, falla uno pero no revise por qué, creo que el test esta bien y es culpa del codigo

--- tests/test_network.c
+++ tests/test_network.c
@@ -3,6 +3,7 @@
 
 #include <signal.h>
 #include <assert.h>
+#include <stdlib.h>
 
 #include "network.h"
 
@@ -52,6 +53,67 @@ START_TEST(test_network_new_destroy)
 END_TEST
 
 /* Testeo de funcionalidad */
+START_TEST(test_network_get_edges)
+{
+    Edge *e1 = NULL, *e2 = NULL, *e3 = NULL, *etmp = NULL;
+    GList *es = NULL, *tmp = NULL;
+    unsigned int i = 0;
+
+    e1 = edge_create(0, 1, 1);
+    e2 = edge_create(0, 2, 1);
+    e3 = edge_create(2, 3, 1);
+
+    net = network_create();
+    network_add_edge(net, e1);
+    network_add_edge(net, e2);
+    network_add_edge(net, e3);
+
+    es = network_get_edges(net, 0);
+
+    tmp = es;
+    i = 1;
+    while(tmp != NULL){
+        etmp = (Edge *) g_list_nth_data(tmp, 0);
+
+        fail_unless(*edge_get_second(etmp) == i);
+        i++;
+
+        tmp = g_list_next(tmp);
+    }
+
+    network_destroy(net);
+}
+END_TEST
+
+START_TEST(test_network_get_neightbours)
+{
+    Edge *e1 = NULL, *e2 = NULL, *e3 = NULL;
+    Node *ntmp = NULL;
+    GList *es = NULL, *tmp = NULL;
+
+    e1 = edge_create(2, 3, 1);
+    e2 = edge_create(1, 4, 1);
+    e3 = edge_create(2, 1, 1);
+
+    net = network_create();
+    network_add_edge(net, e1);
+    network_add_edge(net, e2);
+    network_add_edge(net, e3);
+
+    es = network_neighbours(net, 2);
+
+    tmp = es;
+    while(tmp != NULL){
+        ntmp = (Node *) g_list_nth_data(tmp, 0);
+
+        fail_unless(*ntmp == 3 || *ntmp == 1);
+
+        tmp = g_list_next(tmp);
+    }
+
+    network_destroy(net);
+}
+END_TEST
 
 /* Armado de la test suite */
 Suite *network_suite(void){
@@ -73,7 +135,8 @@ Suite *network_suite(void){
     suite_add_tcase(s, tc_creation);
 
     /* Funcionalidad */
-    /* tcase_add_test(tc_functionality, test_get_weight); */
+    tcase_add_test(tc_functionality, test_network_get_edges);
+    tcase_add_test(tc_functionality, test_network_get_neightbours);
     suite_add_tcase(s, tc_functionality);
 
     return s;
@@ -83,6 +146,26 @@ Suite *network_suite(void){
 void network_memory_test(void){
     /* Codigo que deberia correr sin memory leaks */
 
+    Edge *e1 = NULL, *e2 = NULL, *e3 = NULL, *etmp = NULL;
+    GList *es = NULL, *tmp = NULL;
+
+    e1 = edge_create(0, 1, 1);
+    e2 = edge_create(0, 2, 1);
+    e3 = edge_create(2, 3, 1);
+
     net = network_create();
+    network_add_edge(net, e1);
+    network_add_edge(net, e2);
+    network_add_edge(net, e3);
+
+    es = network_get_edges(net, 0);
+
+    tmp = es;
+    while(tmp != NULL){
+        etmp = (Edge *) g_list_nth_data(tmp, 0);
+        edge_pprint(etmp);
+        tmp = g_list_next(tmp);
+    }
+
     network_destroy(net);
 }
