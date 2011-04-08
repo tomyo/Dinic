77dfbc092a02b25406045a310ea5fde9a72a6ccf
Tomas Hayes <tomyhayes@gmail.com>
2011-04-07 23:05:51 -0300

    Para que andy compile

--- bfs.h
+++ bfs.h
@@ -2,13 +2,19 @@
 #define BFS_H
 
 #include "network.h"
-#include <glib/glist.h>
+#include <glib/glib.h>
 
 /*Corre bfs sobre el network self
   Source: Node s
   Sink: Node t
   */
-GList *execute(Network self, Node s, Node t)
+
+typedef struct s_bfs_result {
+    Glist *path;
+    Weight flow;
+} bfs_result;
+
+bfs_result bfs(Network *self, Node s, Node t);
 
 
 #endif
