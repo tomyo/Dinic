d5206110e4b68fc6c0b170c03cb915e2dc127194
j0hn <j0hn.com.ar@gmail.com>
2011-04-07 22:05:38 -0300

    mas tests y un comentario re util en network.h

--- network.h
+++ network.h
@@ -13,7 +13,9 @@ Network *network_create(void);
 /* Agregar arista al network */
 void network_add_edge(Network *self, Edge *e);
 
-/* Devuelve la lista con punteros a los vecinos del nodo n */
+/* Devuelve la lista con punteros a los vecinos del nodo n
+ * La lista devuelta por esta funcion debe ser liberada
+ * por el que la llame */
 GList *network_neighbours(Network *self, Node n);
 
 /* Devuelve todas las aristas que salen desde n */
