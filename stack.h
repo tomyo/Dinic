48e8dc1150d07083590eb933e160366446b05100
Tomas Hayes <tomyhayes@gmail.com>
2011-06-01 22:12:49 -0300

    Agregando stack.h

--- stack.h
+++ stack.h
@@ -4,7 +4,8 @@
 typedef Stack Queue;
 
 #define stack_new() queue_new()
-#define stack_destroy(stack) queue_free(stack)
+#define stack_free(stack) queue_free(stack)
+#define stack_is_empty(stack) queue_is_empty(stack)
 #define stack_push(stack, elem) queue_push_head(stack, elem)
 #define stack_pop(stack) queue_pop_head(stack)
 
