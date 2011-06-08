bc6191ff4f439fff5cd78b00b43db287c97534e4
j0hn <j0hn.com.ar@gmail.com>
2011-06-07 23:19:07 -0300

    mas avanzando con fix

--- network.c
+++ network.c
@@ -53,22 +53,20 @@ static void destroy_slist_internal(void *list, bool aux_mode) {
         {
             SList *neighbours = current;
             Edge *last = slist_last(neighbours);
-            Node n = edge_get_first(n);
-            bool stop = false;
+            Node *n = edge_get_first(last);
 
-
-            while (!stop || !slist_is_empty(neighbours)) {
-                Edge *head = slist_head_data(neighbours);
-
-                if (edge_get_first(head) != n) {
-                    neighbours = slist_next(neighbours);
-                } else {
-                    stop = true;
-                }
+            /* Avanzo hasta no dejar mas lados backward */
+            while (*edge_get_first(slist_head_data(neighbours)) != *n)  {
+                neighbours = slist_next(neighbours);
             }
 
-
+            /* Ahora neighbours tiene todos lados forward  */
+            while(!slist_is_empty(neighbours)) {
+                edge_destroy(slist_head_data(neighbours));
+                neighbours = slist_next(neighbours);
+            }
         }
+
         /* </FIX> */
 
         while (current != NULL) {
